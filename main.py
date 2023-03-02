6b0aeea444d80aaac593355836be60663099828f
lolsanti <55632755+lolsanti@users.noreply.github.com>
2023-03-02 19:00:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#my_str = 'qwertyuio'
+#print(my_str[2])
+#print(my_str[-2])
+#print(my_str[0:5]) #ноль не нужно писать
+#print(my_str[0:6]) #print(my_str[0:-2]) так точнее
+#print(my_str[::2])
+#print(my_str[1::2])
+#print(my_str[::-1])
+#print(len(my_str))
+
+############################################
+
+#my_str = "this is my first number"
+#count = len(my_str) - my_str.count(" ")
+#print(count)
+
+############################################
+
+#s = input("введи рядок: ")
+#ch = input("введи символ: ")
+
+#for i in range(len(s)):
+    #if s[i] == ch:
+        #print(i)
+
+#############################################
+
+#my_str = 'hhhhh'
+#my_str = my_str[0] + my_str[1:-1].replace('h', 'H') + my_str[-1]
+#print(my_str)  # индийский код
